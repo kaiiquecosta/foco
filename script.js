a93f9b192e76d933ee69a8deec31f540984cd32c
Kaique Costa <137114051+kaiiquecosta@users.noreply.github.com>
2024-03-22 11:58:49 -0300

    Update script.js
    ./

--- script.js
+++ script.js
@@ -59,17 +59,17 @@ function alterarContexto(contexto) {
     switch (contexto) {
         case "foco":
             titulo.innerHTML = `Otimize sua produtividade,<br>
-            <strong class="app__title-strong">mergulhe no que importa.</strong>`    
+            <strong class="app__title-strong">mergulhe no que importa.<./strong>`    
             break;
         
         case "descanso-curto":
             titulo.innerHTML = `Que tal dar uma respirada?<br>
-            <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            <strong class="app__title-strong">Faça uma pausa curta!<./strong>`
             break;
 
         case "descanso-longo":
             titulo.innerHTML = `Hora de voltar à superfície.<br>
-            <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+            <strong class="app__title-strong">Faça uma pausa longa.<./strong>`
             break;    
         default:
             break;
